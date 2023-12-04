5f52b1650d0ae48301c664e5b1e8465188fa87d3
Abhijit Tawde <55575522+abhijeetat9@users.noreply.github.com>
2023-12-04 13:21:08 -0500

    Update

--- 01-JS/easy/anagram.js
+++ 01-JS/easy/anagram.js
@@ -1,16 +1,13 @@
 function isAnagram(str1,str2)
 {
-    let l1 = str1.length;
-    let l2 = str2.length;
+    let l1 = str1.toLowerCase().split("").sort().join("");
+    let l2 = str2.toLowerCase().split("").sort().join("");
 
     if(l1 != l2)
     {
         return false;
     }
-        
-    str1.sort();
-    str2.sort();
-
+    
     for(let i=0;i<l1;i++)
     {
         if(str1[i] != str2[i])
@@ -18,13 +15,4 @@ function isAnagram(str1,str2)
     }
     return true;
 }
-
-let str1 = ['l','i','s','t','e','n'];
-let str2 = ['s','i','l','e','n','t'];
-
-if(isAnagram(str1,str2))
-console.log("The strings are Anagram");
-else
-console.log("the string is not an Anagram");
-
 module.exports = isAnagram;
